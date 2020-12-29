64cef231389a6b5274ca17b23aac2a27006268ae
Zakaria <zakPro@live.be>
2020-12-29 12:13:00 +0100

    exo-fini

---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+
+// let phrase ='Bonjour tout le monde      '.length
+// console.log(phrase);
+
+// let phrase2 ='Bonjour tout le monde      '
+// console.log(phrase2.trim());
+
+// let phrase3 ='Bonjour tout le monde      '
+// console.log(phrase3);
+
+// let phrase = 'Bonjour tout le monde      '
+// console.log(phrase.slice(20));
+
+
+// let phrase = 'Bonjour tout le monde'
+// console.log(phrase.substr(7));
+
+
+// let phrase ='tout le monde'
+// console.log(phrase.replace("tout","Bonjour tout"));
+
+
+
+// let phrase ='Bonjour tout le monde'
+// console.log(phrase.replace("Bonjour tout le monde", "Bonjour"));
+
+// let phrase ='Bonjour tout le monde'
+// console.log(phrase.substr(-21, 7));
+
+
+// let phrase2 = "Ceci est une variable de type number"
+// console.log(phrase2.replace("number", "string"));
+
+
+let phrase2 = "Ceci est une variable de type number"
+console.log(phrase2.toLowerCase())
+
